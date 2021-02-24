59a6591c614c0fcceae1310158ea09a6f239ada6
Lucas <lucasdsveras@gmail.com>
2021-02-24 16:46:34 +0000

    Run sub-analyses of the bland-altman plot data

--- code/04_sub_analyses.R
+++ code/04_sub_analyses.R
@@ -1,22 +1,37 @@
-# Load packages -----------------------------------------------------------
+# Load packages and functions ---------------------------------------------
 
 library(here)
 library(tidyverse)
 library(lvmisc)
+source(here("code", "funs.R"))
 
 # Load and prepare data ---------------------------------------------------
 
 load(here("output", "loocv_data.rda"))
 load(here("data", "mechanical_load_data.rda"))
 
+# Bland-Altman plots tests ------------------------------------------------
+
+# Linear regression diff ~ mean
+ba_regression_res_GRF <- map(cv_res_GRF_models, bland_altman_regression)
+ba_regression_ver_GRF <- map(cv_ver_GRF_models, bland_altman_regression)
+ba_regression_res_LR <- map(cv_res_LR_models, bland_altman_regression)
+ba_regression_ver_LR <- map(cv_ver_LR_models, bland_altman_regression)
+
+# Check if bias differs from 0
+ba_bias_test_res_GRF <- map(cv_res_GRF_models, bland_altman_t_test)
+ba_bias_test_ver_GRF <- map(cv_ver_GRF_models, bland_altman_t_test)
+ba_bias_test_res_LR <- map(cv_res_LR_models, bland_altman_t_test)
+ba_bias_test_ver_LR <- map(cv_ver_LR_models, bland_altman_t_test)
+
 # Accuracy per activity ---------------------------------------------------
 
 # Walking
-cv_res_GRF_models %>%
+walking_accuracy <- cv_res_GRF_models %>%
   map(~ filter(.x, speed %in% 1:6)) %>%
   map(accuracy, na.rm = TRUE)
 # Running
-cv_res_GRF_models %>%
+running_accuracy <- cv_res_GRF_models %>%
   map(~ filter(.x, speed %in% 7:14)) %>%
   map(accuracy, na.rm = TRUE)
 
