2de0d12bef55c0a2e3c03ef8ff585c383f5ec91c
Lucas <lucasdsveras@gmail.com>
2021-02-25 18:20:22 +0000

    Format table to render as pdf

--- tables/tab1.R
+++ tables/tab1.R
@@ -2,7 +2,8 @@
 
 library(here)
 library(tidyverse)
-library(lvmisc)
+library(broman)
+library(kableExtra)
 source(here("code", "funs.R"))
 
 # Load and prepare data ---------------------------------------------------
@@ -19,15 +20,32 @@ ver_GRF_tb <- build_formula_table(
   ver_GRF_models, cv_ver_GRF_models, "pGRF", "vertical"
 )
 
-tab1 <- rbind(res_GRF_tb, ver_GRF_tb)
+tab1 <- rbind(res_GRF_tb, ver_GRF_tb) %>%
+  mutate(
+    MAPE = paste0(myround(MAPE * 100, 2), "\\%")
+  )
 # Reorder table rows
 tab1 <- tab1[c(1, 4, 2, 5, 3, 6), ]
 # Remove placemente name duplicates
 tab1[c(2, 4, 6), 1] <- ""
 
-# Save the table tibble ---------------------------------------------------
 
-if (!dir.exists(here("output"))) {
-  dir.create(here("output"))
-}
-save(tab1, file = here("output", "tab1.rda"))
+tab1 <- kbl(
+  tab1, booktabs = TRUE, escape = FALSE, linesep = "",
+  col.names = c(
+    linebreak("Accelerometer\nplacement"),
+    "Regression equations",
+    "$R^2$",
+    "MAE",
+    "MAPE",
+    "RMSE"
+  ),
+  align = c(rep("l", 2), rep("r", 4))
+) %>%
+  kable_styling(latex_options = "scale_down") %>%
+  pack_rows(
+    "Peak ground reaction force prediction", 1, 6,
+    latex_gap_space = "0.5em",
+    bold = FALSE
+  ) %>%
+  landscape()
